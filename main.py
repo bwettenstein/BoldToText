d9ffab11448440fb4eb1e2999193246b9fee98c4
bwettenstein <admin@Arnold.local>
2019-11-25 23:15:00 -0800

    Removed blank spaces to align with PEP8

--- main.py
+++ main.py
@@ -18,6 +18,5 @@ if __name__ == "__main__":
                                   custom_text='No Path Chosen',
                                   button_type=gui.Ok())
 
-
     text_list = get_text(doc_file)
     write_doc(text_list, file_name)
